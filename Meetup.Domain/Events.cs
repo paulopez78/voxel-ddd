647bca770fcc2c3041986352d4301c6d966cd222
paulopez78 <pau.lopez.78@gmail.com>
2019-07-10 21:58:52 +0200

    add aggregate id to events

--- Meetup.Domain/Events.cs
+++ Meetup.Domain/Events.cs
@@ -7,31 +7,37 @@ namespace Meetup.Domain
     {
         public class MeetupCreated
         {
+            public Guid MeetupId { get; set; }
             public string Title { get; set; }
             public string Location { get; set; }
         }
 
         public class MeetupPublished
         {
+            public Guid MeetupId { get; set; }
         }
 
         public class NumberOfSeatsUpdated
         {
+            public Guid MeetupId { get; set; }
             public int NumberOfSeats { get; set; }
         }
 
         public class MeetupCanceled
         {
+            public Guid MeetupId { get; set; }
         }
 
         public class RSVPAccepted
         {
+            public Guid MeetupId { get; set; }
             public Guid MemberId { get; set; }
             public DateTime AcceptedAt { get; set; }
         }
 
         public class RSVPDeclined
         {
+            public Guid MeetupId { get; set; }
             public DateTime DeclinedAt { get; set; }
             public Guid MemberId { get; set; }
         }
